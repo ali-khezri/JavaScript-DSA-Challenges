cf535da69d97b1ef6b15ca5a139eab0a92f40bd1
Ali Khezri <khezri.ali.1994@gmail.com>
2025-12-13 17:33:26 +0330

    Challenge 8 Done second solution !

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,9 +1,47 @@
+// function isPalindrome(str) {
+//   const formatStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+//   const reverseArr = formatStr.split("").reverse().join("");
+
+//   return reverseArr === formatStr;
+// }
+
 function isPalindrome(str) {
-  const formatStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const formattedStr = removeNonAlphanumeric(str.toLowerCase());
+  const rev = reverseString(formattedStr);
+  return rev === formattedStr;
+}
 
-  const reverseArr = formatStr.split("").reverse().join("");
+function removeNonAlphanumeric(str) {
+  let formatStr = "";
 
-  return reverseArr === formatStr;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (isAlphaNumeric(char)) {
+      formatStr += char;
+    }
+  }
+  return formatStr;
+}
+
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+
+  return (
+    (code >= 48 && code <= 57) || // Numbers 0-9
+    (code >= 97 && code <= 122) // Lowercase letters a-z
+  );
+}
+
+function reverseString(str) {
+  let reverseStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+
+  return reverseStr;
 }
 
 module.exports = isPalindrome;
