245f8797f22b5bc69512db598b1c2859bfc7f144
Ali Khezri <khezri.ali.1994@gmail.com>
2025-12-14 21:04:16 +0330

    Challenge 09 Done!

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,30 @@
-function countVowels() {}
+function countVowels(str) {
+  const formatStr = str.toLowerCase();
+
+  let count = 0;
+  for (let i = 0; i < formatStr.length; i++) {
+    const element = formatStr[i];
+
+    if (
+      element === "a" ||
+      element === "e" ||
+      element === "i" ||
+      element === "o" ||
+      element === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
+
+// a e i o u
+
+// test("Counting vowels in a string", () => {
+//   expect(countVowels("Hello, World!")).toBe(3);
+//   expect(countVowels("JavaScript")).toBe(3);
+//   expect(countVowels("OpenAI Chatbot")).toBe(6);
+//   expect(countVowels("Coding Challenge")).toBe(5);
+// });
--- README.md
+++ README.md
@@ -52,9 +52,9 @@ Feedback and alternative solutions are always welcome.
 | 04  | Count Occurrences          | ✅     |
 | 05  | Find Max Number            | ✅     |
 | 06  | Title Case                 | ✅     |
-| 07  | Reverse String             | ✅      |
+| 07  | Reverse String             | ✅     |
 | 08  | Palindrome                 | ✅     |
-| 09  | Count Vowels               | ⭕     |
+| 09  | Count Vowels               | ✅     |
 | 10  | Remove Duplicates          | ⭕     |
 
 ---
