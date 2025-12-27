e5984fd734f42e75ce455588a5eb76a9d10f692a
Ali Khezri <khezri.ali.1994@gmail.com>
2025-12-27 14:42:05 +0330

    Challenge 17 Done!

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -17,4 +17,20 @@ function findFirstNonRepeatingCharacter(str) {
   return null;
 }
 
+// function findFirstNonRepeatingCharacter(str) {
+//   const char = new Map();
+
+//   for (const element of str) {
+//     char.set(element, (char.get(element) || 0) + 1);
+//   }
+
+//   for (const element of str) {
+//     if (char.get(element) === 1) {
+//       return element;
+//     }
+//   }
+
+//   return null;
+// }
+
 module.exports = findFirstNonRepeatingCharacter;
