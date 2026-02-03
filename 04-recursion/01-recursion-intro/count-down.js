269a3924b10cd8c9449ef28630e7f722b17b6389
Ali Khezri <khezri.ali.1994@gmail.com>
2026-02-03 16:50:28 +0330

    Challenge 31 Done!

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,13 @@
-function countDown() {}
+function countDown(num) {
+  // Base case
+  if (num <= 0) {
+    console.log("Well done!");
+    return;
+  }
+  // Recursive case
+  console.log(num);
+  num--;
+  countDown(num);
+}
 
 module.exports = countDown;
